38d188c215c20dc58021da74948f963413e35157
tianyiYoung <550448928@qq.com>
2017-04-02 22:04:21 +0800

    user表定义以及sql脚本

---
+++ database.sql
@@ -0,0 +1,18 @@
+#create database BookHi
+
+use bookhi;
+
+create table user(
+	userid     INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+	username   varchar(20)  NOT NULL,
+	pwd        varchar(20)  NOT NULL,
+	nickname   varchar(30),
+	city         varchar(100),                     
+	lasttime     datetime,                     
+	registertime  datetime,
+	phone        VARCHAR(15),
+	email        VARCHAR(30),
+	isValid      TINYINT  DEFAULT 1,
+	lastLoginIp  VARCHAR(30),
+	primary      key(userid)
+)ENGINE=InnoDB; 
